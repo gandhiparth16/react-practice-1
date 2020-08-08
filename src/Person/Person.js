1088f14115eed28fbc1af6299f0885da4e568e5e
gandhiparth16 <69316051+gandhiparth16@users.noreply.github.com>
2020-08-09 00:19:29 +0530

    Update Person.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,17 +1,27 @@
 import React from "react";
+import "./Person.css";
 
 const person = (props) => {
   let isHobby = null;
   let hobby = props.children; //{props.children} cannot be used in conditional statement
   if (hobby) {
-    isHobby = ". Hobby is" + hobby;
+    isHobby = `Hobby : ${hobby}`;
   }
   return (
-    /*to add multiple lines inside return statement, use ();
-              return multiple lines in output, use div to wrap. */
-    <h5>
-      Hi {props.name} aged {props.age} {isHobby}
-    </h5>
+    <div className="Person">
+      {/*to add multiple lines inside return statement, use ();
+              return multiple lines in output, use div to wrap. */}
+      <h5 onClick={props.click}>
+        {/*/function click from class App */}
+        Hi {props.name} aged {props.age} years. {isHobby}
+      </h5>
+      <input
+        id="Input"
+        type="text"
+        onChange={props.changed}
+        value={props.name}
+      />
+    </div>
   );
 };
 
